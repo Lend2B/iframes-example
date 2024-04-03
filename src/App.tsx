d49ff70e8e2b7df066541c2b3bfd4fa34c5bed2a
Carlos Chacon <carlosdchaconmendez@gmail.com>
2024-04-03 16:44:14 -0400

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Amplify } from "aws-amplify";
 import { IframePages } from "./Pages/IframesPage";
-
-const queryClient = new QueryClient();
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: import.meta.env["VITE_COGNITO_USER_POOL_ID"] ?? "",
-      userPoolClientId:
-        import.meta.env["VITE_COGNITO_USER_POOL_CLIENT_ID"] ?? "",
-    },
-  },
-});
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<IframePages />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<IframePages />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
