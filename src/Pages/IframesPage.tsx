171d11b23ee12653372269b2de7ed68c89f453e5
Carlos Chacon <carlosdchaconmendez@gmail.com>
2024-10-14 10:50:21 -0400

    Add selection functionality to IframesPage for Credit Status and Credit Portfolio

--- src/Pages/IframesPage.tsx
+++ src/Pages/IframesPage.tsx
@@ -13,6 +13,7 @@ interface Props {
 
 export const IframePages = () => {
   const [token, setToken] = useState<string>("");
+  const [selected, setSelected] = useState(0);
   const iframeId = useId();
 
   const getAccessToken = async (values: Props) => {
@@ -45,8 +46,8 @@ export const IframePages = () => {
         <Formik
           initialValues={{
             email: "callao-abl@yopmail.com",
-            password: "r@jop9i*WiB",
-            entity_email: "client-stage@yopmail.com",
+            password: "Demo123*",
+            entity_email: "client-dev@yopmail.com",
           }}
           validationSchema={SignInValidation}
           onSubmit={mutate}
@@ -80,38 +81,74 @@ export const IframePages = () => {
     );
   }
 
+  if (selected === 1) {
+    return (
+      <section className="w-full h-screen flex  justify-center items-center p-10">
+        <div className="flex flex-col items-center  w-5/6 h-full">
+          <iframe
+            id={iframeId + "-creditStatus"}
+            src={`${
+              import.meta.env["VITE_ENDPOINT"]
+            }/CreditStatus?token=${token}`}
+            height="180px"
+            width="1100px"
+            // className="w-full"
+          />
+          <iframe
+            id={iframeId + "-extensionsReport"}
+            src={`${
+              import.meta.env["VITE_ENDPOINT"]
+            }/ExtensionsReport?token=${token}`}
+            width="1100px"
+            height="500px"
+            // className="w-full h-4/6"
+          />
+        </div>
+        <div className="h-full ">
+          <iframe
+            id={iframeId + "-nextExpirations"}
+            src={`${
+              import.meta.env["VITE_ENDPOINT"]
+            }/NextExpirations?token=${token}`}
+            width="300px"
+            height="420px"
+            className="m-auto"
+          />
+        </div>
+      </section>
+    );
+  }
+
+  if (selected === 2) {
+    return (
+      <section className="w-full h-screen  p-5">
+        <iframe
+          id={iframeId + "-creditPortfolio"}
+          src={`${
+            import.meta.env["VITE_ENDPOINT"]
+          }/CreditPortfolioUpload?token=${token}`}
+          className="m-auto w-full h-full"
+        />
+      </section>
+    );
+  }
+
   return (
-    <section className="w-full h-screen flex  justify-center items-center p-10">
-      <div className="flex flex-col items-center  w-5/6 h-full">
-        <iframe
-          id={iframeId + "-creditStatus"}
-          src={`${
-            import.meta.env["VITE_ENDPOINT"]
-          }/CreditStatus?token=${token}`}
-          height="180px"
-          width="1000px"
-          // className="w-full"
-        />
-        <iframe
-          id={iframeId + "-extensionsReport"}
-          src={`${
-            import.meta.env["VITE_ENDPOINT"]
-          }/ExtensionsReport?token=${token}`}
-          width="1000px"
-          height="500px"
-          // className="w-full h-4/6"
-        />
-      </div>
-      <div className="h-full ">
-        <iframe
-          id={iframeId + "-nextExpirations"}
-          src={`${
-            import.meta.env["VITE_ENDPOINT"]
-          }/NextExpirations?token=${token}`}
-          width="300px"
-          height="420px"
-          className="m-auto"
-        />
+    <section className="w-full h-screen flex flex-col gap-3 items-center justify-center">
+      <h1 className="font-semibold text-2xl">Select an option</h1>
+      <div className="flex gap-3 w-full justify-center">
+        <button
+          className="bg-blue-500 p-2 w-1/6 rounded-lg text-white hover:bg-blue-700 transition-all ease-in-out duration-150 mt-1"
+          onClick={() => setSelected(1)}
+        >
+          Credit Status
+        </button>
+        <button
+          className="bg-blue-500 p-2 w-1/6 rounded-lg text-white hover:bg-blue-700 transition-all ease-in-out duration-150 mt-1"
+          onClick={() => setSelected(2)}
+        >
+          Credit Portfolio
+        </button>
       </div>
     </section>
   );
